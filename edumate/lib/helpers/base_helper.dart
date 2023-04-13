ba7541a904edcf322f597c1cdd24b54d0e66612c
visalatas <94117032+visalatas@users.noreply.github.com>
2023-04-13 23:59:24 +0300

    generic yapı

--- edumate/lib/helpers/base_helper.dart
+++ edumate/lib/helpers/base_helper.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class BaseHelper {
-  void mesajGoster(BuildContext context, String mesaj) {
+
+  void mesajGoster(BuildContext context, String mesaj, [String title ='İşlem Sonucu']) {
+
     var alert = AlertDialog(
-      title: Text('İşlem Sonucu'),
+      title: Text(title),
       content: Text(mesaj),
     );
     showDialog(
